9678e7e100f777c024ef8fbd09b0a55d9262b778
siegaii <siiegaiii@gmail.com>
2023-07-17 18:25:27 +0800

    chore: ci

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,16 @@
 image_name="ai-tools-srv"
 
-# 停止并删除 Docker 容器
 if docker ps -aq -f name=$image_name > /dev/null; then
     if docker inspect -f '{{.State.Running}}' $image_name > /dev/null; then
         docker stop $image_name > /dev/null
     fi
     docker rm $image_name > /dev/null
-    echo "已删除容器 $image_name。"
+    echo "Removed container: $image_name。"
 fi
 
-# 删除 Docker 镜像
 if docker images --format "{{.Repository}}" | grep -q "^$image_name$"; then
     docker rmi $image_name > /dev/null
-    echo "已删除镜像 $image_name。"
+    echo "Removed image: $image_name。"
 fi
 
 docker build -t $image_name .
@@ -23,6 +21,6 @@ docker run -itd \
 --name $image_name \
 $image_name
 
-echo "已启动容器 $image_name。"
+echo "Container Starting $image_name。"
 
 docker rmi $(docker images | grep '<none>' | awk '{print $3}')
